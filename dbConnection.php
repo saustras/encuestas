c2984d777b71836b00984c8f397585a492bdd29a
saustras <72366423+saustras@users.noreply.github.com>
2023-02-11 18:12:36 -0500

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -5,4 +5,4 @@ $DB_USER = $_ENV["DB_USER"];
 $DB_PASSWORD = $_ENV["DB_PASSWORD"];
 $DB_NAME = $_ENV["DB_NAME"];
 $DB_PORT = $_ENV["DB_PORT"];
-$con = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD = $_ENV["DB_PASSWORD"], $DB_NAME = $_ENV["DB_NAME"], $DB_PORT = $_ENV["DB_PORT"]) or die("Could not connect to mysql" . mysqli_error($con));
+$con = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_PORT) or die("Could not connect to mysql" . mysqli_error($con));
