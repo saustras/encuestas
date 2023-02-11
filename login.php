df71572ecc2a8ae90d45a99457f67571dbb5b0be
saustras <72366423+saustras@users.noreply.github.com>
2023-02-11 18:45:46 -0500

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@ session_start();
 if(isset($_SESSION["email"])){
 session_destroy();
 }
-include_once 'dbConnection.php';
+include_once '/app/dbConnection.php';
 $ref=@$_GET['q'];
 $email = $_POST['email'];
 $password = $_POST['password'];
