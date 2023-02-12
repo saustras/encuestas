6c5d15fa5ed9f0b69fb03bab6f52aff016781cf9
saustras <72366423+saustras@users.noreply.github.com>
2023-02-11 23:05:23 -0500

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -8,5 +8,5 @@ $DB_PORT = $_ENV["DB_PORT"];
 $con  = mysqli_init();
 $con->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
 $con->real_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
-$con->close();
+
 
