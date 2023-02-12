d2318fa4edc1c9ad64dc20b36a25633e8dbce6f2
saustras <72366423+saustras@users.noreply.github.com>
2023-02-11 23:14:07 -0500

    Update dbConnection.php

--- dbConnection.php
+++ dbConnection.php
@@ -5,4 +5,5 @@ $DB_PASSWORD = $_ENV["DB_PASSWORD"];
 $DB_NAME = $_ENV["DB_NAME"];
 
 $con = new mysqli($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME) or die("Could not connect to mysql" . mysqli_error($con));
+$con->ssl_set(NULL, NULL, "/etc/ssl/certs/ca-certificates.crt", NULL, NULL);
 
